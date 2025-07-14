758857374ed5606e27993b74a62ddf0c1483067b
vikas <vkvishwaji@gmail.com>
2025-07-14 16:25:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+
+
+const app = express()
+const port =  4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('Hello Twitter!')
+})
+app.get('/chai', (req, res) => {
+  res.send('<h1 style="color: green;">Chai Aur Code</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
