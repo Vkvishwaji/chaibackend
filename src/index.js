74a22f96ed1ef199cd5687ea3021c9de2f30d058
vikas <vkvishwaji@gmail.com>
2025-07-15 21:08:59 +0530

    mongoDB connected

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-require('dotenv').config()
-const express = require('express')
+//require("dotenv").config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({
+  path: './env'
+});
 
-const app = express()
-const port =  4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/twitter', (req, res) => {
-  res.send('Hello Twitter!')
-})
-app.get('/chai', (req, res) => {
-  res.send('<h1 style="color: green;">Chai Aur Code</h1>')
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+connectDB()
+ 
+ 
